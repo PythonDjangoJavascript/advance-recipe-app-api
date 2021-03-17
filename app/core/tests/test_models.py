44b1246691b7e2f4a7bb4e4d890b55d8ebf785b0
alpha <nuruddinsayeed@gmail.com>
2021-03-17 22:31:19 +0600

    edited tests for user model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -3,6 +3,7 @@ from django.contrib.auth import get_user_model
 
 
 class ModelTests(TestCase):
+    """All tests about users database"""
 
     def test_create_user_with_email_successful(self):
         """Test creating a new user with an email is successful"""
